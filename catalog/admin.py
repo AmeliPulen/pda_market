f1d63fda5b69045a7302df3e7569819be2711fd9
AmeliPulen <139628158+AmeliPulen@users.noreply.github.com>
2024-04-06 15:41:14 +0300

    Added solution for homework 20.1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'category', 'price',)
+    list_filter = ('category', 'price',)
+    search_fields = ('name', 'category', 'price',)
